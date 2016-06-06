65e29b298a63602d6f27be5f22e628a532f01088
Administrator <heli@gjk365.com>
2016-06-06 14:18:40 +0800

    新建SoapUI 与Jenkins持续集成项目

---
+++ Soapui/src/main/java/Soapui/App.java
@@ -0,0 +1,17 @@
+package Soapui;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        
+    }
+}
---
+++ Soapui/src/test/java/Soapui/AppTest.java
@@ -0,0 +1,68 @@
+package Soapui;
+
+import java.io.File;
+import java.io.IOException;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public AppTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( AppTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+        File dir = new File("");
+        /**
+         * 案例报告存放路径
+         */
+        String reportPath="D:\\temp\\soapui_reports";
+        /**
+         * Soapui案例脚本存放路径
+         */
+        String path =dir.getAbsolutePath()+"\\prject_xml\\";
+        /**
+         * 调用testrunner.bat批处理命令
+         */
+        String common =
+//        "cmd /D soapui.bat";
+        		"cmd /D "
+        		+"cd D:\\Program Files\\SmartBear\\SoapUI-Pro-5.1.2\\bin"
+        		+ " D:\\Program Files\\SmartBear\\SoapUI-Pro-5.1.2\\bin\\testrunner.bat -r -j " + path +"报告单列表.xml -f " + reportPath +" -FPDF";
+        System.out.println("报告存放路径:"+reportPath);
+        System.out.println("common: "+common);
+        try{
+        	Process process = Runtime.getRuntime().exec(common);
+        	System.out.println("done");
+        	
+        }catch(IOException e){
+        	e.printStackTrace();
+        	
+        }
+    }
+}
